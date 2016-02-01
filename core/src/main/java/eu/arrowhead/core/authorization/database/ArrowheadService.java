7b4fc1cc33d01fc13118d42ee39d4030255b3c56
blevente92 <bujdosolevente@gmail.com>
2016-02-01 23:42:03 +0100

    Revert "Revert "Some work on the authorization.""
    This reverts commit 37fc2122c7173829d15fb9d7e7bbac679527ecb5.

--- core/src/main/java/eu/arrowhead/core/authorization/database/ArrowheadService.java
+++ core/src/main/java/eu/arrowhead/core/authorization/database/ArrowheadService.java
@@ -6,12 +6,15 @@ import java.util.List;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 public class ArrowheadService {
 
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @XmlTransient
 	private int id;
 	private String serviceGroup;
 	private String serviceDefinition;
--- core/src/main/java/eu/arrowhead/core/orchestrator/OrchestrationResource.java
+++ core/src/main/java/eu/arrowhead/core/orchestrator/OrchestrationResource.java
@@ -3,7 +3,10 @@ package eu.arrowhead.core.orchestrator;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("orchestration")
 public class OrchestrationResource {
@@ -12,5 +15,21 @@ public class OrchestrationResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "This is the Orchestration Service";
+    }
+    
+    /*
+     * Testing communication between core systems
+     */
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testMethod(){
+    	Client client = ClientBuilder.newClient();
+    	
+    	Response response = client.target("http://localhost:8080/core/authorization/operator/A/cloud/b")
+    			.request().get();
+    	String message = response.readEntity(String.class);
+    	
+    	return message;
     }
 }
