804f2f00c675177c8783ee925aa2d3d707c1f6e0
Balázs Pardavi <pardavib@gmail.com>
2016-02-11 21:15:24 +0100

    QoS messages created

---
+++ core/src/main/java/eu/arrowhead/common/model/messages/QoSReserve.java
@@ -0,0 +1,43 @@
+package eu.arrowhead.common.model.messages;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import eu.arrowhead.common.model.ArrowheadService;
+import eu.arrowhead.common.model.ArrowheadSystem;
+
+@XmlRootElement
+public class QoSReserve {
+
+	private ArrowheadSystem provider;
+	private ArrowheadSystem consumer;
+	private ArrowheadService service;
+
+	public QoSReserve() {
+
+	}
+
+	public ArrowheadSystem getProvider() {
+		return provider;
+	}
+
+	public void setProvider(ArrowheadSystem provider) {
+		this.provider = provider;
+	}
+
+	public ArrowheadSystem getConsumer() {
+		return consumer;
+	}
+
+	public void setConsumer(ArrowheadSystem consumer) {
+		this.consumer = consumer;
+	}
+
+	public ArrowheadService getService() {
+		return service;
+	}
+
+	public void setService(ArrowheadService service) {
+		this.service = service;
+	}
+
+}
