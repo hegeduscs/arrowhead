6654c7ec20863a0969bbe4438bf738ebbe25b141
blevente92 <bujdosolevente@gmail.com>
2016-02-16 21:56:43 +0100

    Small changes in GSDAnswer.

--- core/src/main/java/eu/arrowhead/common/model/messages/GSDAnswer.java
+++ core/src/main/java/eu/arrowhead/common/model/messages/GSDAnswer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eu.arrowhead.common.model.ArrowheadCloud;
-import eu.arrowhead.common.model.ArrowheadService;
 
 public class GSDAnswer {
 
