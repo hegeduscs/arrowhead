662956db92ed9f5039a962c48809f806201fc187
Sandor Plosz <plosz@arrowhead.tmit.bme.hu>
2017-02-23 10:49:27 +0100

    add firewall export, scripts can only be run as root

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -1,3 +1,9 @@
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 /etc/init.d/arrowhead.api start
 /etc/init.d/arrowhead.authorization start
 /etc/init.d/arrowhead.gatekeeper start
--- scripts/stopall.sh
+++ scripts/stopall.sh
@@ -1,3 +1,9 @@
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 /etc/init.d/arrowhead.api stop
 /etc/init.d/arrowhead.authorization stop
 /etc/init.d/arrowhead.gatekeeper stop
