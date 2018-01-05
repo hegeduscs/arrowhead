d251c8d6f5b4a8d7ce9e5ec5ba294abf28b896ff
Umlauf Zoltán <zoltan.umlauf@gmail.com>
2018-01-05 15:46:47 +0100

    added a new exception mapper for json mapping exceptions

---
+++ common/src/main/java/eu/arrowhead/common/exception/JsonMappingExceptionMapper.java
@@ -0,0 +1,19 @@
+package eu.arrowhead.common.exception;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
+
+  @Override
+  public Response toResponse(JsonMappingException ex) {
+    ex.printStackTrace();
+    ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 400, JsonMappingException.class.toString());
+
+    return Response.status(Response.Status.BAD_REQUEST).entity(errorMessage).build();
+  }
+
+}
