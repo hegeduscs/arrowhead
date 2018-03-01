4ecfbaa900076a7760be1a792dd0a2e780f4bba8
Umlauf Zoltán <zoltan.umlauf@gmail.com>
2018-03-01 12:54:15 +0100

    shell scripts added

---
+++ start_insecure_arrowhead.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#run as sudo
+
+echo Starting Arrowhead Core Systems - wait 15-20 seconds
+
+cd target
+
+if [ -e nohup.out ]
+then
+    rm nohup.out
+fi
+
+nohup java -jar arrowhead_core-M3-lightweight.jar -d -daemon -m insecure &
+sleep 20s
+
+echo Arrowhead is running... Check nohup.out to make sure exceptions did not occur!
---
+++ stop_arrowhead.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#run as sudo
+pkill -f arrowhead_core
+sleep 3s
+echo Arrowhead Core Systems killed
