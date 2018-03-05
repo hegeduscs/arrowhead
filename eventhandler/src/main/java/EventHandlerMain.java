15be25ad24d9d0173787b872a3487c095d28a8ac
Umlauf Zoltán <zoltan.umlauf@gmail.com>
2018-03-05 11:42:53 +0100

    M4-dev initial commit with the new core system modules

---
+++ eventhandler/src/main/java/EventHandlerMain.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 AITIA International Inc.
+ *
+ * This work is part of the Productive 4.0 innovation project, which receives grants from the
+ * European Commissions H2020 research and innovation programme, ECSEL Joint Undertaking
+ * (project no. 737459), the free state of Saxony, the German Federal Ministry of Education and
+ * national funding authorities from involved countries.
+ */
+
+public class EventHandlerMain {
+
+  public static void main(String[] args) {
+
+  }
+
+}
