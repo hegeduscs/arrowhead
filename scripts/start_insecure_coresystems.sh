e28534319c90fc13966f404d337654472561da86
uzoltan <zoltan.umlauf@gmail.com>
2018-03-28 17:09:03 +0200

    starter scripts updated

--- scripts/start_insecure_coresystems.sh
+++ scripts/start_insecure_coresystems.sh
@@ -6,31 +6,26 @@ echo Starting Core Systems - wait 1 minute
 #More sleep time between core systems might be needed on slower devices like a Raspberry Pi
 
 cd serviceregistry_sql/target
-rm nohup.out
-nohup java -jar serviceregistry_sql-M3.jar -d -daemon -m insecure &
+nohup java -jar serviceregistry_sql-M3.jar -d -daemon > insecure_sr.log &
 echo Service Registry started
 sleep 10s
 
 cd ../../authorization/target
-rm nohup.out
-nohup java -jar authorization-M3.jar -d -daemon -m insecure &
+nohup java -jar authorization-M3.jar -d -daemon > insecure_auth.log &
 echo Authorization started
 sleep 10s
 
 cd ../../gateway/target
-rm nohup.out
-nohup java -jar gateway-M3.jar -d -daemon -m insecure &
+nohup java -jar gateway-M3.jar -d -daemon > insecure_gateway.log &
 echo Gateway started
 sleep 10s
 
 cd ../../gatekeeper/target
-rm nohup.out
-nohup java -jar gatekeeper-M3.jar -d -daemon -m insecure &
+nohup java -jar gatekeeper-M3.jar -d -daemon > insecure_gk.log &
 echo Gatekeeper started
 sleep 10s
 
 cd ../../orchestrator/target
-rm nohup.out
-nohup java -jar orchestrator-M3.jar -d -daemon -m insecure &
+nohup java -jar orchestrator-M3.jar -d -daemon > insecure_orch.log &
 echo Orchestrator started
 sleep 10s
--- scripts/start_secure_coresystems.sh
+++ scripts/start_secure_coresystems.sh
@@ -6,31 +6,26 @@ echo Starting Core Systems - wait 1 minute
 #More sleep time between core systems might be needed on slower devices like a Raspberry Pi
 
 cd serviceregistry_sql/target
-rm nohup.out
-nohup java -jar serviceregistry_sql-M3.jar -d -daemon -m secure &
+nohup java -jar serviceregistry_sql-M3.jar -d -daemon -tls > secure_sr.log &
 echo Service Registry started
 sleep 10s
 
 cd ../../authorization/target
-rm nohup.out
-nohup java -jar authorization-M3.jar -d -daemon -m secure &
+nohup java -jar authorization-M3.jar -d -daemon -tls > secure_auth.log &
 echo Authorization started
 sleep 10s
 
 cd ../../gateway/target
-rm nohup.out
-nohup java -jar gateway-M3.jar -d -daemon -m secure &
+nohup java -jar gateway-M3.jar -d -daemon -tls > secure_gateway.log &
 echo Gateway started
 sleep 10s
 
 cd ../../gatekeeper/target
-rm nohup.out
-nohup java -jar gatekeeper-M3.jar -d -daemon -m secure &
+nohup java -jar gatekeeper-M3.jar -d -daemon -tls > secure_gk.log &
 echo Gatekeeper started
 sleep 10s
 
 cd ../../orchestrator/target
-rm nohup.out
-nohup java -jar orchestrator-M3.jar -d -daemon -m secure &
+nohup java -jar orchestrator-M3.jar -d -daemon -tls > secure_orch.log &
 echo Orchestrator started
 sleep 10s
