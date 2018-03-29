37c931e8bbc9cccdbe6668be78d846ab882ea04a
uzoltan <zoltan.umlauf@gmail.com>
2018-03-29 14:17:58 +0200

    stop script updated with forcekill option

--- stop_arrowhead.sh
+++ stop_arrowhead.sh
@@ -2,4 +2,11 @@
 #run as sudo
 pkill -f arrowhead_core
 sleep 3s
-echo Arrowhead Core Systems killed
+
+if pgrep -f arrowhead_core
+then
+  kill -KILL $(ps aux | grep 'arrowhead_core' | awk '{print $2}')
+  echo Arrowhead Core Systems forcefully killed
+else
+  echo Arrowhead Core Systems killed
+fi
