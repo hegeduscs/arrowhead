35354f6c87d5ba3f5d3cddf5cf376fd4a5fd24ad
uzoltan <zoltan.umlauf@gmail.com>
2018-05-24 13:11:46 +0200

    shell scripts updated

--- scripts/start_secure_coresystems.sh
+++ scripts/start_secure_coresystems.sh
@@ -5,27 +5,32 @@ cd ..
 echo Starting Core Systems - wait 1 minute
 #More sleep time between core systems might be needed on slower devices like a Raspberry Pi
 
-cd serviceregistry_sql/target
-nohup java -jar serviceregistry_sql-M4.jar -d -daemon -tls > secure_sr.log &
+cd ../serviceregistry_sql/target
+nohup java -jar serviceregistry_sql-4.0.jar -d -daemon -tls > secure_sr.log &
 echo Service Registry started
 sleep 10s
 
 cd ../../authorization/target
-nohup java -jar authorization-M4.jar -d -daemon -tls > secure_auth.log &
+nohup java -jar authorization-4.0.jar -d -daemon -tls > secure_auth.log &
 echo Authorization started
 sleep 10s
 
 cd ../../gateway/target
-nohup java -jar gateway-M4.jar -d -daemon -tls > secure_gateway.log &
+nohup java -jar gateway-4.0.jar -d -daemon -tls > secure_gateway.log &
 echo Gateway started
 sleep 10s
 
+cd ../../eventhandler/target
+nohup java -jar eventhandler-4.0.jar -d -daemon -tls > secure_eventhandler.log &
+echo Event Handler started
+sleep 10s
+
 cd ../../gatekeeper/target
-nohup java -jar gatekeeper-M4.jar -d -daemon -tls > secure_gk.log &
+nohup java -jar gatekeeper-4.0.jar -d -daemon -tls > secure_gk.log &
 echo Gatekeeper started
 sleep 10s
 
 cd ../../orchestrator/target
-nohup java -jar orchestrator-M4.jar -d -daemon -tls > secure_orch.log &
+nohup java -jar orchestrator-4.0.jar -d -daemon -tls > secure_orch.log &
 echo Orchestrator started
 sleep 10s
