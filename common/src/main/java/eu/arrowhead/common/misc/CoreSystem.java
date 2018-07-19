be1e6f8c6b2facb8ed2f72b7543e2cd92de471f0
uzoltan <zoltan.umlauf@gmail.com>
2018-07-19 11:09:00 +0200

    -

--- common/src/main/java/eu/arrowhead/common/misc/CoreSystem.java
+++ common/src/main/java/eu/arrowhead/common/misc/CoreSystem.java
@@ -19,8 +19,8 @@ public enum CoreSystem {
   AUTHORIZATION(8444, 8445, Arrays.asList("db_user", "db_password", "db_address", "keystore", "keystorepass"),
                 Arrays.asList("keypass", "truststore", "truststorepass"), Arrays.asList(CoreSystemService.AUTH_CONTROL_SERVICE, CoreSystemService.TOKEN_GEN_SERVICE)),
   CHOREOGRAPHER(8456, 8457, null, null, null),
-  EVENT_HANDLER(8454, 8455, ArrowheadMain.dbFields, ArrowheadMain.certFields,
-                Arrays.asList(CoreSystemService.EVENT_PUBLISH, CoreSystemService.EVENT_SUBSCRIPTION)),
+  EVENTHANDLER(8454, 8455, ArrowheadMain.dbFields, ArrowheadMain.certFields,
+               Arrays.asList(CoreSystemService.EVENT_PUBLISH, CoreSystemService.EVENT_SUBSCRIPTION)),
   GATEKEEPER_INTERNAL(8446, 8447, ArrowheadMain.dbFields, Arrays
       .asList("gatekeeper_keystore", "gatekeeper_keystore_pass", "gatekeeper_keypass", "cloud_keystore", "cloud_keystore_pass", "cloud_keypass",
               "master_arrowhead_cert"), null),
@@ -33,8 +33,8 @@ public enum CoreSystem {
   QOS(8450, 8451, Arrays.asList("db_user", "db_password", "db_address", "monitor_url"), ArrowheadMain.certFields, null),
   SERVICE_REGISTRY_DNS(8442, 8443, null, ArrowheadMain.certFields, null),
   SERVICE_REGISTRY_SQL(8442, 8443, ArrowheadMain.dbFields, ArrowheadMain.certFields, null),
-  SYSTEM_REGISTRY(8436, 8437, ArrowheadMain.dbFields, ArrowheadMain.certFields, Collections.singletonList(CoreSystemService.SYS_REG_SERVICE)),
-  DEVICE_REGISTRY(8438, 8439, ArrowheadMain.dbFields, ArrowheadMain.certFields, Collections.singletonList(CoreSystemService.DEVICE_REG_SERVICE));
+  SYSTEMREGISTRY(8436, 8437, ArrowheadMain.dbFields, ArrowheadMain.certFields, Collections.singletonList(CoreSystemService.SYS_REG_SERVICE)),
+  DEVICEREGISTRY(8438, 8439, ArrowheadMain.dbFields, ArrowheadMain.certFields, Collections.singletonList(CoreSystemService.DEVICE_REG_SERVICE));
 
   private final int insecurePort;
   private final int securePort;
