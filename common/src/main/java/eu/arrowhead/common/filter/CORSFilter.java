3bab045e90d1c40265c320042f1b66d09343f00a
uzoltan <zoltan.umlauf@gmail.com>
2018-07-19 10:55:44 +0200

    -

--- common/src/main/java/eu/arrowhead/common/filter/CORSFilter.java
+++ common/src/main/java/eu/arrowhead/common/filter/CORSFilter.java
@@ -9,7 +9,6 @@
 
 package eu.arrowhead.common.filter;
 
-import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -19,7 +18,7 @@ import javax.ws.rs.ext.Provider;
 public class CORSFilter implements ContainerResponseFilter {
 
   @Override
-  public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+  public void filter(ContainerRequestContext request, ContainerResponseContext response) {
     response.getHeaders().add("Access-Control-Allow-Origin", "*");
     response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
     response.getHeaders().add("Access-Control-Allow-Credentials", "true");
