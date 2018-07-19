14de185968fb5601fa466c3c4aab8237966eb034
uzoltan <zoltan.umlauf@gmail.com>
2018-07-19 11:15:02 +0200

    -

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/EventHandlerMain.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/EventHandlerMain.java
@@ -28,7 +28,7 @@ public class EventHandlerMain extends ArrowheadMain {
   private EventHandlerMain(String[] args) {
     Set<Class<?>> classes = new HashSet<>(Collections.singleton(EventHandlerResource.class));
     String[] packages = {"eu.arrowhead.common", "eu.arrowhead.core.eventhandler.filter"};
-    init(CoreSystem.EVENT_HANDLER, args, classes, packages);
+    init(CoreSystem.EVENTHANDLER, args, classes, packages);
 
     //if removing old filters (based on endDate field) is requested, start the TimerTask that provides it
     if (props.getBooleanProperty("remove_old_filters", false)) {
--- systemregistry/src/main/java/eu/arrowhead/core/systemregistry/SystemRegistryMain.java
+++ systemregistry/src/main/java/eu/arrowhead/core/systemregistry/SystemRegistryMain.java
@@ -19,7 +19,7 @@ public class SystemRegistryMain extends ArrowheadMain {
 	private SystemRegistryMain(String[] args) {
 		Set<Class<?>> classes = new HashSet<>(Collections.singleton(SystemRegistryResource.class));
 		String[] packages = { "eu.arrowhead.common", "eu.arrowhead.SystemRegistry.filter" };
-		init(CoreSystem.SYSTEM_REGISTRY, args, classes, packages);
+		init(CoreSystem.SYSTEMREGISTRY, args, classes, packages);
 
 		listenForInput();
 	}
